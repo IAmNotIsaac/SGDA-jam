e1946cf2714dd5d05ffc0a160e3468b287b43874
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-08-15 21:10:45 -0700

    added controller support

--- src/instance/player.gd
+++ src/instance/player.gd
@@ -11,6 +11,7 @@ const _GRAVITY := 9.8
 const _JUMP_FORCE := 4.0
 const _RUN_SPEED := 5.0
 const _AIR_FRICTION := 0.2
+const _CONTROLLER_SENSITIVITY := 5.0
 
 var _state := StateMachine.new(self, States)
 var _velocity := Vector3.ZERO
@@ -35,35 +36,51 @@ func _input(event : InputEvent) -> void:
 
 func _physics_process(delta : float) -> void:
 	_state.process(delta)
+	_controller_look()
+
+
+func _controller_look() -> void:
+	var input := Vector2(
+		Input.get_action_strength("look_right") - Input.get_action_strength("look_left"),
+		Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
+	)
+	
+	_n_gimbal.rotation_degrees.y -= input.x * _CONTROLLER_SENSITIVITY
+	_n_cam.rotation_degrees.x -= input.y * _CONTROLLER_SENSITIVITY
+	_n_cam.rotation_degrees.x = clamp(_n_cam.rotation_degrees.x, -90, 90)
 
 
 ## State processes ##
 
 
 func _sp_DEFAULT(_delta : float) -> void:
-	var forward_dir := Input.get_axis("move_forward", "move_backward")
-	var strafe_dir := Input.get_axis("move_left", "move_right")
+	var input := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	)
 	
 	var theta = _n_gimbal.global_transform.basis.get_euler().y
 	
-	var move_forward = Vector3(sin(theta) * forward_dir, 0.0, cos(theta) * forward_dir)
-	var move_strafe = Vector3(cos(-theta) * strafe_dir, 0.0, sin(-theta) * strafe_dir)
+	var move_forward = Vector3(sin(theta) * input.y, 0.0, cos(theta) * input.y)
+	var move_strafe = Vector3(cos(-theta) * input.x, 0.0, sin(-theta) * input.x)
 	_velocity = (move_forward + move_strafe).normalized() * _RUN_SPEED
 	
-	move_and_slide(_velocity)
+	_velocity = move_and_slide(_velocity)
 	
 	if Input.is_action_pressed("jump"):
 		_state.switch(States.JUMP)
 
 
 func _sp_AIR(delta : float) -> void:
-	var forward_dir := Input.get_axis("move_forward", "move_backward")
-	var strafe_dir := Input.get_axis("move_left", "move_right")
+	var input := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	)
 	
 	var theta = _n_gimbal.global_transform.basis.get_euler().y
 	
-	var move_forward = Vector3(sin(theta) * forward_dir, 0.0, cos(theta) * forward_dir)
-	var move_strafe = Vector3(cos(-theta) * strafe_dir, 0.0, sin(-theta) * strafe_dir)
+	var move_forward = Vector3(sin(theta) * input.y, 0.0, cos(theta) * input.y)
+	var move_strafe = Vector3(cos(-theta) * input.x, 0.0, sin(-theta) * input.x)
 	
 	_velocity = Vector3(
 		clamp(_velocity.x + (move_forward.x + move_strafe.x) * _AIR_FRICTION, -_RUN_SPEED, _RUN_SPEED),
@@ -71,7 +88,7 @@ func _sp_AIR(delta : float) -> void:
 		clamp(_velocity.z + (move_forward.z + move_strafe.z) * _AIR_FRICTION, -_RUN_SPEED, _RUN_SPEED)
 	)
 	
-	move_and_slide(_velocity, Vector3.UP)
+	_velocity = move_and_slide(_velocity, Vector3.UP)
 	
 	if is_on_floor():
 		_state.switch(States.DEFAULT)
